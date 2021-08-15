47dc0acd0aa692b0105c57918f00810757c7d233
Karan Kumar <Kumarkay.ca@gmail.com>
2021-08-15 02:50:53 -0400

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Karan Kumar on 2021-08-15.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
