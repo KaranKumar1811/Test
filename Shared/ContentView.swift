eedfb9bd57c3c6c9dba6c3b02ba2865f8d2c2546
Karan Kumar <Kumarkay.ca@gmail.com>
2021-08-15 03:29:53 -0400

    Init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+        Text("Hello, Karan")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .bold()
             .padding()
+            .autocapitalization(.allCharacters)
+        
+        Text("Bro")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .bold()
+            .padding()
+            .autocapitalization(.allCharacters)
+        
+            
+        }
+        
+        
+            
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                
+            
+        }
     }
 }
