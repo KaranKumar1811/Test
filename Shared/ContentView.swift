1e22ee3acdf597d71ea1bd91ae2f4cecdfcc3bb2
Karan Kumar <Kumarkay.ca@gmail.com>
2021-08-30 02:14:16 -0400

    init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,36 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+struct ContentView: View
+{
+    var body: some View
+       {
+       // Text("World")
         VStack {
-        Text("Hello, Karan")
-            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            .bold()
-            .padding()
-            .autocapitalization(.allCharacters)
-        
-        Text("Bro")
-            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            .bold()
-            .padding()
-            .autocapitalization(.allCharacters)
-        
-            
+            Text("Hello, Karan")
+                .bold()
+                .font(.custom("Bebas Neue", size: 50))
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                
+                
+            Text("Good Morning")
+                .font(.title)
+                .bold()
+                .padding()
+                .autocapitalization(.allCharacters)
+
+            myVstack.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.blue)
+                }
         }
-        
-        
-            
-        
+    
+    
+    let myVstack = VStack
+     {
+         VStack
+         {
+             Text("Text 1")
+             Text("Text 2")
+               
+             HStack
+             {
+                 Text("Text 3")
+                 Text("Text 4")
+             }
+         }
+         Text("Text 5")
+
+     Text("How ")+Text("are ")+Text("you?")
+
+     }
+}
+
+
+struct MyFont: ViewModifier
+{
+    func body(content: Content) -> some View
+    {
+        content
+            .font(.title)
+            .padding()
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        Group
+        {
             ContentView()
-                
-            
         }
     }
 }
